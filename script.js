a7485405476c3fad0ebb9dd34545b6c6061ba50e
ykmsd <yuka1110m@gmail.com>
2024-04-24 22:30:51 +0200

    fix

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 console.log('Hello!');
 
 async function test() {
-    try {
-      const response = await fetch("https://echo.zuplo.io/foo?bar=1");
-      const test = await response.json();
-      console.log('test', test);
-    } catch {
-      console.log('error');
-    }
-
+    // try {
+    //   const response = await fetch("https://echo.zuplo.io/foo?bar=1");
+    //   const test = await response.json();
+    //   console.log('test', test);
+    // } catch {
+    //   console.log('error');
+    // }
   }
 
   test();
