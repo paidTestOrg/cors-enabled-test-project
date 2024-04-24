fe20976d494a3d3ff280c0248b9b975cc1f1a3d0
ykmsd <yuka1110m@gmail.com>
2024-04-24 18:52:10 +0200

    fix url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Hello!');
 
 async function shiba() {
-    const response = await fetch("http://shibe.online/api/shibes");
+    const response = await fetch("https://shibe.online/api/shibes");
     const shiba = await response.json();
     console.log(shiba);
   }
