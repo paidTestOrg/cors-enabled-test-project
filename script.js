3f8e7138bee98b0d0dc3fca936b1c233d2550ab4
ykmsd <yuka1110m@gmail.com>
2024-04-24 19:46:32 +0200

    update url

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('Hello!');
 
 async function test() {
     try {
-      const response = await fetch("https://google.com");
+      const response = await fetch("https://echo.zuplo.io/foo?bar=1");
       const test = await response.json();
       console.log('test', test);
     } catch {
