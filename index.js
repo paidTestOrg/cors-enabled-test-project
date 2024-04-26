b35e929ce1c8588bdd8e1ec2c267d685a5ccf07e
ykmsd <yuka1110m@gmail.com>
2024-04-26 16:33:30 +0200

    update

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-//Load HTTP module
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  test();
-  res.end('Hello World\n');
-});
-
-async function test() {
+async function corsTest() {
   try {
-    const response = await fetch('https://archive.org/download/win3_Mineswee/win3_Mineswee.zip');
+    const response = await fetch(
+      'https://archive.org/download/win3_Mineswee/win3_Mineswee.zip'
+    );
     console.log(response.headers.get('content-type'));
+
+    const res = await fetch('https://echo.zuplo.io/foo?bar=1');
+    const json = await res.json();
+    console.log(json.query.bar);
   } catch (e) {
     console.log('error', e);
   }
 }
 
-test();
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+corsTest();
